d514f1edb989b3a1c7bb292d50c98408400c4e1c
DHEERAJ KUMAR <98643857+CYB3RPHO3NIX@users.noreply.github.com>
2024-04-07 00:30:11 +0530

    RangeFilter and DoesNot filter

---
+++ CrossProcedures/RangeFilter.sql
@@ -0,0 +1,22 @@
+﻿CREATE PROCEDURE [cp].[RangeFilter]
+    @SchemaName NVARCHAR(100),
+    @TableName NVARCHAR(100),
+    @ColumnName NVARCHAR(100),
+    @lValue NVARCHAR(MAX),
+    @hValue NVARCHAR(MAX)
+AS
+BEGIN
+    DECLARE @SqlQuery NVARCHAR(MAX);
+
+    -- Build the dynamic SQL query
+    SET @SqlQuery = '
+        SELECT * 
+        FROM ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName) + '
+        WHERE ' + QUOTENAME(@ColumnName) + ' >= @lValue 
+        AND ' + QUOTENAME(@ColumnName) + ' <= @hValue';
+
+    -- Execute the dynamic SQL query
+    EXEC sp_executesql @SqlQuery, 
+        N'@lValue NVARCHAR(MAX), @hValue NVARCHAR(MAX)', 
+        @lValue, @hValue;
+END
