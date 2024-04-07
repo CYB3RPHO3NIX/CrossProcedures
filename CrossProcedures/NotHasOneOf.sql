218d8120d0535292e85b356a83b080913875a036
DHEERAJ KUMAR <98643857+CYB3RPHO3NIX@users.noreply.github.com>
2024-04-07 08:47:11 +0530

    Added one more filter

---
+++ CrossProcedures/NotHasOneOf.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [cp].[NotHasAnyOfFilter]
+(
+    @SchemaName NVARCHAR(128),
+    @TableName NVARCHAR(128),
+    @ColumnName NVARCHAR(128),
+    @SearchString NVARCHAR(MAX)
+)
+AS
+BEGIN
+    DECLARE @sqlQuery NVARCHAR(MAX)
+    SET @sqlQuery = '
+        SELECT *
+        FROM ' + QUOTENAME(@SchemaName) + '.' + QUOTENAME(@TableName) + '
+        WHERE ' + QUOTENAME(@ColumnName) + ' NOT IN (''' + REPLACE(@SearchString, '|', ''',''') + ''')
+    '
+
+    EXEC sp_executesql @sqlQuery
+END
